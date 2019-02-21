231ddf27425910a0afd8682a14dc66fc4b0dd91e
System Administrator <root@tests-MacBook-Pro.local>
2019-02-21 18:21:12 +0100

    Updates

---
+++ precache-manifest.01d4069047839a8faad565b76329ee0a.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "8e6c3ede4ec197ecf69b",
+    "url": "/Prezentacja_Strony/static/js/runtime~main.8e6c3ede.js"
+  },
+  {
+    "revision": "bcef9a8d53a483154a5b",
+    "url": "/Prezentacja_Strony/static/js/main.bcef9a8d.chunk.js"
+  },
+  {
+    "revision": "2b2d12eea82ef053751d",
+    "url": "/Prezentacja_Strony/static/js/2.2b2d12ee.chunk.js"
+  },
+  {
+    "revision": "9f24fa06d73bf02c0da0e2523d88ba41",
+    "url": "/Prezentacja_Strony/index.html"
+  }
+];
